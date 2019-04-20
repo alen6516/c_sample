61d13745260769a34f85f31858a25ede06118a31
alen6516 <alen6516@gmail.com>
2019-04-20 22:38:25 +0800

    add itoa.c

---
+++ itoa.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *itoa(int num) {
+	int len;
+	int tmp = num;
+	for(len = 0; tmp != 0; len++, tmp/=10);
+	char *res = (char*)malloc(sizeof(char)*(len+1));
+	for(int i=1; i<len+1; i++) {
+		*(res+i) = num%10+'0';
+		num /= 10;
+	}
+	*res = 0;
+
+	int t;
+	for(int i=0; i<(len+1)/2; i++) {
+		t = *(res+i);
+		*(res+i) = *(res+len-i);
+		*(res+len-i) = t;
+	}
+
+	return res;
+}
+
+int main() {
+	printf("%s\n", itoa(12345));
+	
+}
