2f0a21e79791bbb3f391224c696ccc410686d35d
alen6516 <alen6516@gmail.com>
2019-09-16 12:40:15 +0800

    update leetcode

--- undergo/8_String_to_Integer.c
+++ undergo/8_String_to_Integer.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int myAtoi(char * s) {
 
     int len = strlen(s);
-    char sign = -1;      // -1 for uninit, 0 for -, 1 for +
-    int ret = 0;
+    char is_positive = -1;      // -1 for uninit, 0 for -, 1 for +
+    int tmp_ret, ret = 0;
 
     for (int i=0; i<len; i++) {
 
-        if (sign == -1) {
+        if (is_positive == -1) {
 
             if (s[i] == ' ')
                 continue;
             
             if (s[i] == '-') {
-                sign = 0;
+                is_positive = 0;
                 continue;
             
             } else if (s[i] == '+') {
-                sign = 1;
+                is_positive = 1;
                 continue;
 
-            } else if (s[i] == '+' || (s[i] >= '0' && s[i] <= '9')) {
-                sign = 1;
+            } else if ((s[i] >= '0' && s[i] <= '9')) {
+                is_positive = 1;
             
             } else {
                 return 0;
@@ -33,15 +34,26 @@ int myAtoi(char * s) {
         if ( !(s[i] >= '0' && s[i] <= '9') )
             break;
 
-        ret = ret * 10 + (s[i] - '0');
+
+        tmp_ret = ret;
+        ret *= 10;
+        ret += (is_positive) ? (s[i] - '0'): 0-(s[i]-'0');
+
+
+        // check if overflow
+        if (  (is_positive && ret < tmp_ret)  ||  
+              (!is_positive && ret > tmp_ret)) {
+            
+            return 0;
+        }
         
     }
-    return (sign == 0) ? 0-ret : ret;
+    return ret;
 }
 
 int main () {
 
-    char s[] = "  5487aabb";
+    char s[] = " -5487aabb";
     printf("input : %s\n", s);
     printf("result: %d\n", myAtoi(s));
     printf("expect: %d\n", 5487);
