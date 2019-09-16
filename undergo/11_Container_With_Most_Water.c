ef2331fe2a79909771e62775e3e562840f748bfa
alen6516 <alen6516@gmail.com>
2019-09-17 00:06:51 +0800

    update lootcode

---
+++ undergo/11_Container_With_Most_Water.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int maxArea(int* height, int heightSize){
+
+}
+
+int main () {
+
+    int arr[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    printf("%d\n", maxArea(arr, 9));
+
+}
