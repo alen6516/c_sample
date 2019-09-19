2766f3d7d7458b57d0f48b7fe6f1c5d04a16ca63
alen6516 <alen6516@gmail.com>
2019-09-19 12:53:11 +0800

    update leetcode

--- undergo/11_Container_With_Most_Water.c
+++ undergo/11_Container_With_Most_Water.c
@@ -2,13 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MIN(a, b)  ((a)<(b) ? (a) : (b))
+
+
 int maxArea(int* height, int heightSize){
 
+    int left_side = 0;
+    int right_side = heightSize-1;
+    int curr_max_area = (heightSize-1) * MIN(height[left_side], height[right_side]);
+
+
+    int curr_area;
+    for (int i=2; i<heightSize; i++) {
+        
+        if (height[left_side] > height[right_side]) {
+            right_side --;
+            printf("now right_side is %d\n", height[right_side]);
+
+        } else {
+            left_side ++;
+            printf("now left_side is %d\n", height[left_side]);
+
+        }
+
+        if (right_side == left_side)
+            break;
+
+        curr_area = (heightSize-i) * MIN(height[left_side], height[right_side]);
+        printf("this area is %d * %d = %d\n", heightSize-i, MIN(height[left_side], height[right_side]), curr_area);
+
+        if (curr_area > curr_max_area) {
+            curr_max_area = curr_area;
+        }
+    }
+
+    return curr_max_area;
 }
 
 int main () {
 
-    int arr[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    int arr[] = {1, 5, 6, 2, 5, 4, 8, 3, 7};
+    printf("arr[] = [1, 5, 6, 2, 5, 4, 8, 3, 7]\n");
     printf("%d\n", maxArea(arr, 9));
 
 }
