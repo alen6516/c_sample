3f2bf383a7f24ffde18c9264671f2e3cb642f03a
alen6516 <alen6516@gmail.com>
2019-09-24 15:20:32 +0800

    update leetcode

--- undergo/11_Container_With_Most_Water.c
+++ undergo/11_Container_With_Most_Water.c
@@ -1,38 +1,41 @@
+/*
+ * Runtime: 12 ms, faster than 95.64% of C online submissions for Container With Most Water.
+ * Memory Usage: 7.9 MB, less than 14.29% of C online submissions for Container With Most Water.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MIN(a, b)  ((a)<(b) ? (a) : (b))
 
+int maxArea(int* height, int heightSize) {
 
-int maxArea(int* height, int heightSize){
-
-    int left_side = 0;
-    int right_side = heightSize-1;
+	if (heightSize == 2) return MIN(height[0], height[1]);
+    
+    unsigned short left_side = 0;
+    unsigned short right_side = (unsigned short) (heightSize-1);
     int ret = (heightSize-1) * MIN(height[left_side], height[right_side]);
 
-
+    int curr_area;
     for (int i=2; i<heightSize; i++) {
-        
+
         if (height[left_side] > height[right_side]) {
             right_side --;
-            //printf("now right_side is %d\n", height[right_side]);
 
         } else {
             left_side ++;
-            //printf("now left_side is %d\n", height[left_side]);
 
         }
 
         if (right_side == left_side)
             break;
 
-        //curr_area = (heightSize-i) * MIN(height[left_side], height[right_side]);
-        //printf("this area is %d * %d = %d\n", heightSize-i, MIN(height[left_side], height[right_side]), curr_area);
+        curr_area = (heightSize-i) * MIN(height[left_side], height[right_side]);
 
-        ret = ( (heightSize-i) * MIN(height[left_side], height[right_side]) > ret ) ? (heightSize-i) * MIN(height[left_side], height[right_side]) : ret;
+        if (curr_area > ret) {
+            ret = curr_area;
+        }
     }
-
     return ret;
 }
 
