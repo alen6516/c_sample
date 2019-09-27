128d3802e82416c9cdb43b0ee98bfc524584dc1d
alen6516 <alen6516@gmail.com>
2019-09-27 18:16:06 +0800

    update leetcode

--- undergo/15_3Sum.c
+++ undergo/15_3Sum.c
@@ -5,7 +5,8 @@
  */
 
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stdlib.h>
 
 /*
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
@@ -13,17 +14,21 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
 }
 */
 
-int min(int a, int b, int c) {
+#define MIN_3(a, b, c) ((a>b) ? ( (b>c) ? c : b ) : ( (a>c) ? c : a))
+#define MAX_3(a, b, c) ((a<b) ? ( (b<c) ? c : b ) : ( (a<c) ? c : a))
 
-    return (a>b) ? ( (b>c) ? c: b ) : ( (a>c) ? c: a);
+#define c_size_get_2(size) size*(size-1)/2
+
+
+int get_idx(int input, int *arr, int total) {
+    for (int i=0; i<total; i++) {
+        if (arr[i] == input) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-#define MIN_3(a, b, c) ((a>b) ? ( (b>c) ? c : b ) : ( (a>c) ? c : a))
-
-
-int arr_1[10];
-int arr_2[10];
-int arr_3[10];
 
 void threeSum(int *nums, int size) {
     
@@ -31,21 +36,34 @@ void threeSum(int *nums, int size) {
 	int f2;
 	int f3;
 
+
+    int this_max, this_min;
+
+    int tmp_idx;
+
+    int total = 0;
+    int arr_min[c_size_get_2(size)];
+    int arr_max[c_size_get_2(size)];
+
 	for (f1 = 0; f1 < size; f1++) { 
         for (f2 = f1+1; f2 < size; f2++) { 
             for (f3 = f2+1; f3 < size; f3++) {
                 if (0 == nums[f1]+nums[f2]+nums[f3]) {
                     
-                    /*
-                    if (check(f1, f2, f3)) {
-                        int *new = (int*) malloc(sizeof(int)*3);
-                        new[0] = f1;
-                        new[1] = f2;
-                        new[2] = f3;
+                    this_max = MAX_3(nums[f1], nums[f2], nums[f3]);
+                    this_min = MIN_3(nums[f1], nums[f2], nums[f3]);
+
+
+                    tmp_idx = get_idx(this_min, arr_min, total);
+                    if (tmp_idx != -1 && arr_max[tmp_idx] == this_max) {
+                        // ignore
+                    } else {
+                        arr_min[total] = this_min;
+                        arr_max[total] = this_max;
+                        total++;
+
+                        printf("min=%d, med=%d, max=%d\n", this_min, 0-this_max-this_min, this_max);
                     }
-                    */
-                    printf("f1=%d:%d, f2=%d:%d, f3=%d:%d\n", f1, nums[f1], f2, nums[f2], f3, nums[f3]);
-                    
                 } 
             }
         }
@@ -54,10 +72,9 @@ void threeSum(int *nums, int size) {
 
 int main () {
 
-	int nums[] = {1, -2, 1, 1, 0, -1};
-	printf("{1, -2, 1, 1, 0, -1}\n");
-	int numsSize = 6;
+	int nums[] = {1, -2, 1, -5, 1, 0, -1, -2, 4, 3};
+	printf("{1, -2, 1, -5, 1, 0, -1, -2, 4, 3}\n");
+	int numsSize = 10;
 		
-
 	threeSum(nums, numsSize);
 }
