5c8e011bdceff360361a265586ba10256cf5edf1
alen6516 <alen6516@gmail.com>
2019-10-02 10:51:45 +0800

    update http_server

--- undergo/http_server/main.c
+++ undergo/http_server/main.c
@@ -7,17 +7,36 @@
 #include <sys/socket.h>     /* socket(), bind(), listen(), ... */
 #include <netinet/in.h>     /* AF_INET, AF_INET6 addr family and their corresponding protocol family PF_INET, PFINET6 */
 #include <arpa/inet.h>      /* hton */
+#include <unistd.h>         /* read(), write(), close() */
 
-void handle_socket(int fd) {
+
+#define PORT 8787
+#define BUF_SIZE 1024
+#define BACKLOG 10          // backlog argument for listen()
 
 
+void *handle_socket(void *_fd) {
+
+
+    int fd = *(int*)_fd;
+    printf("receive a client, fd = %d\n", fd);
+    char buf[BUF_SIZE];
+    bzero(buf, BUF_SIZE);
+
+
+    strncpy(buf, "hello clien\n", BUF_SIZE);
+    write(fd, buf, BUF_SIZE);
+    close(fd);
+    pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
 
-    int listenfd, socketfd;
-    static struct sockaddr_in cli_addr;
+    int listenfd;
     static struct sockaddr_in serv_addr;
+
+    int socketfd;                           // for accept
+    static struct sockaddr_in cli_addr;     // for accept
     socklen_t addr_len;
 
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -26,19 +45,24 @@ int main (int argc, char *argv[]) {
     }
 
    
-    // network configuration
-    serv_addr.sin_family = AF_INET;
+    // Setup server addr
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = PF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(80);
+    serv_addr.sin_port = htons(PORT);
+
 
     // bind
     if (bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        exit(3);
+        perror("bind failed");
+        exit(1);
     }
 
+
     // listen
-    if (listen(listenfd, 64) < 0) {
-        exit(3);
+    if (listen(listenfd, BACKLOG) < 0) {
+        perror("listen failed");
+        exit(1);
     }
 
 
@@ -46,11 +70,12 @@ int main (int argc, char *argv[]) {
         addr_len = sizeof(cli_addr);
 
         if ((socketfd = accept(listenfd, (struct sockaddr *) &cli_addr, &addr_len)) < 0) {
-            exit(3);
+            perror("accept fail");
+            exit(1);
         } 
        
 
         pthread_t work_thr;
-        pthread_create(&work_thr, NULL, handle_socket, socketfd);
+        pthread_create(&work_thr, NULL, handle_socket, (void*) &socketfd);
     }
 }
