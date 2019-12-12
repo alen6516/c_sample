6a494a578490e1b98e17273ef36b2445797a2ea4
alen6516 <alen6516@gmail.com>
2019-12-12 19:27:54 +0800

    add chat_roo,

---
+++ undergo/chat_room/README.md
@@ -0,0 +1,12 @@
+* TCP chat room
+* once enter the room, user need to log their name
+
+* directory structure
+    * main.c
+    * define.h
+    * config.h
+    * func.h
+* data structure
+    * fd => array
+    * conn => hash_table
+    * parse_buf + thr_list => thr_node_list
---
+++ undergo/chat_room/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>                 // access()
+#include <assert.h>
+#include <time.h>                   // time_t, time()
+#include <errno.h>
+#include <signal.h>                 // signal()
+#include <pthread.h>
+
+#include <sys/socket.h>             /* socket(), bind(), listen(), ... */
+#include <netinet/in.h>             /* AF_INET, AF_INET6 addr family and their corresponding protocol family PF_INET, PFINET6 */
+#include <arpa/inet.h>              /* hton(), inet_ntop() */
+#include <unistd.h>                 /* read(), write(), close() */
+
+
+int main () {
+
+    
+
+}
