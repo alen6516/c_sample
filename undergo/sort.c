3ace9065954f448e8142d685ea6e355b942e7f3a
alen6516 <alen6516@gmail.com>
2019-12-30 20:20:22 +0800

    add sort.c

---
+++ undergo/sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SWAP(a,b) do {      \
+    a = a^b;                \
+    b = a^b;                \
+    a = a^b;                \
+} while(0);
+
+
+void sort(int *arr, int len) {
+
+    int tmp;
+
+    for (int i=1; i<len; i++) {
+        for (int j=0; j<i; j++) {
+            if (arr[i]<arr[j]) {
+                if (i-1 == j) {
+                    SWAP(arr[i], arr[j]);
+                } else {
+                    tmp = arr[i];
+                    memmove()
+                }
+            }
+        }
+    } 
+}
+
+int main () {
+
+    int arr[] = {2, 1, 4, 9, 8, 6};
+    int len = 6;
+
+    sort(arr, len);
+}
