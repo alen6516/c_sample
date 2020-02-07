b906cbfac744d2526b998ff4158e8cce36624a70
alen6516 <alen6516@gmail.com>
2020-02-07 09:53:27 +0800

    update _16

--- undergo/_16_3Sum_Closest.c
+++ undergo/_16_3Sum_Closest.c
@@ -7,23 +7,119 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LEN 3
+
+#define ARR_LEN(arr) ((int)(sizeof(arr)/sizeof(arr[0])))
+
 #define abs(a) ((a)>0 ? (a) : (0-(a)))
 
+#define SWAP(a, b) do {     \
+    a = a^b;                \
+    b = a^b;                \
+    a = a^b;                \
+} while (0);
 
+int partition (int arr[], int left, int right) {
+
+    int mid_idx = right;
+    right--;
+    int swap_with_mid_before_return = -1;
+
+
+    for (;left <= right; left++) {
+        if (arr[left] > arr[mid_idx]) {     // if this one is larger then mid
+
+            swap_with_mid_before_return = left;
+
+            // find a small one from right
+            for (; right > left ; right--) {
+                if (arr[right] < arr[mid_idx]) {
+
+                    // if find, then swap them
+                    SWAP(arr[left], arr[right]);
+                    swap_with_mid_before_return = right;
+                    break;
+                }
+            }
+        } 
+    }
+
+    if (swap_with_mid_before_return != -1) {
+        SWAP(arr[swap_with_mid_before_return], arr[mid_idx]);
+        return swap_with_mid_before_return;
+    } else {
+        return right;
+    }
+}
+
+void quick_sort(int arr[], int left, int right) {
+
+    int q;
+
+    q = partition(arr, left, right);
+    
+    if (q != left && q != left+1)
+        quick_sort(arr, left, q);
+    if (q != right && q != right-1)
+        quick_sort(arr, q, right);
+}
 
 int threeSumClosest(int* nums, int numsSize, int target) {
 
-    if(numsSize == 3)
+    if(numsSize == 3) {
         return nums[0]+nums[1]+nums[2];
+    }
 
-
+    quick_sort(nums, 0, numsSize-1);
     
+    int left=0, mid = 1, right = numsSize-1;
+    int curr_sum;
+
+    int cont = 1;
+    int check_if_too_large = 1;
+
+    while(cont && right>mid && mid>left) {
+        curr_sum = nums[left]+nums[mid]+nums[right];
+        if (curr_sum-target > 0) {
+            if (check_if_too_large) {
+                right--;
+                mid = left+1;
+                continue;
+            } else {
+                break;
+            }
+        } else if (curr_sum-target < 0) {
+            if (!check_if_too_large) {
+                left++;
+                mid = right-1;
+                continue;
+            } else {
+                check_if_too_large = 0;
+                mid = right-1;
+                continue;
+            }
+        }
+    }
+
+    for (int i=left; i<=right; i++) {
+        printf("%d, ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
 
 int main () {
-    int nums[] = {-1, 2, 1, -4};
-    int target = 1;
-    threeSumClosest(nums, 4, target);
+    int nums[] = {-50, -40, -39, -20, -15, -14, 0, 10, 20, 100, 200};
+    int len = ARR_LEN(nums);
+    for (int i=0; i<len; i++) {
+        printf("%d, ", nums[i]);
+    }
+    printf("\n");
 
+    quick_sort(nums, 0, len-1);
+
+    int target = 1;
+    threeSumClosest(nums, len, target);
 }
