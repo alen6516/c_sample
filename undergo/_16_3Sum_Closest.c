69503f11e470554f31c4060f925962ef57a300d0
alen6516 <alen6516@gmail.com>
2020-09-28 00:28:25 +0800

    update

--- undergo/_16_3Sum_Closest.c
+++ undergo/_16_3Sum_Closest.c
@@ -17,13 +17,23 @@
     a = a^b;                \
 } while (0)
 
+int len;
+void show(int arr[]) {
+    for (int i=0; i<len; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+}
+
+
 int partition (int arr[], int left, int right) {
+    printf("in this partition, left = %d, right = %d\n", left, right);
 
     int mid_idx = right;
     right--;
     int swap_with_mid_before_return = -1;
 
-    for (;left <= right; left++) {
+    for (;left < right; left++) {
         if (arr[left] > arr[mid_idx]) {     // if this one is larger then mid
 
             swap_with_mid_before_return = left;
@@ -34,6 +44,7 @@ int partition (int arr[], int left, int right) {
 
                     // if find, then swap them
                     SWAP(arr[left], arr[right]);
+                    show(arr);
                     swap_with_mid_before_return = right;
                     break;
                 }
@@ -43,6 +54,8 @@ int partition (int arr[], int left, int right) {
 
     if (swap_with_mid_before_return != -1) {
         SWAP(arr[swap_with_mid_before_return], arr[mid_idx]);
+        printf("swap before return\n");
+        show(arr);
         return swap_with_mid_before_return;
     } else {
         return right;
@@ -51,18 +64,29 @@ int partition (int arr[], int left, int right) {
 
 void quick_sort(int arr[], int left, int right) {
 
+
+    if (right == left+1) {
+        if (arr[left] > arr[right]) {
+            SWAP(arr[left], arr[right]);
+        }
+        return;
+    }
+
     int q;
 
     q = partition(arr, left, right);
+    printf("partition return %d\n", q);
     
-    if (q != left && q != left+1)
+
+    if (q != left)
         quick_sort(arr, left, q);
-    if (q != right && q != right-1)
+    if (q != right)
         quick_sort(arr, q, right);
 }
 
 
 // compare if it is a smaller diff
+/*
 void check_and_replace(int target, int left, int mid, int right, int nums[], int *diff) {
 
     int is_change = 0;
@@ -79,13 +103,25 @@ void check_and_replace(int target, int left, int mid, int right, int nums[], int
         printf("idx %d %d %d make new closest sum %d, the new diff is %d\n", left, mid, right, sum, *diff);
     }
 }
+*/
+#define check_and_replace(target, left, mid, right, nums, diff) ({  \
+    int is_change = 0;                                              \
+    int sum;                                                        \
+    sum = nums[left] + nums[mid] + nums[right];                     \
+    if (abs(*diff) > abs(target-sum)) {                             \
+        *diff = sum-target;                                         \
+        is_change = 1;                                              \
+    }                                                               \
+    if (is_change) {                                                \
+        printf("idx %d %d %d make new closest sum %d, the new diff is %d\n", left, mid, right, sum, *diff); \
+    }                                                               \
+})
 
 // walk through every element from left to right and check
 void walk_through(int nums[], int left, int right, int target, int *diff) {
     if (right - left < 2) {
         return ;
     }
-
 
     printf("walk start\n");
     for (int mid = left +1; mid<right; mid++) {
@@ -99,6 +135,7 @@ void walk_through(int nums[], int left, int right, int target, int *diff) {
 
 void cut(int nums[], int left, int right, int target, int *diff) {
 
+    /*
     if (right-left < 2) return;
     else if (right-left == 2) {
         check_and_replace(target, left, left+1, right, nums, diff);
@@ -106,10 +143,10 @@ void cut(int nums[], int left, int right, int target, int *diff) {
             return ;
         }
     }
+    */
 
     int mid=left+1;
     int curr_sum;
-    int mid_from_left = 1;
 
     while(right > mid && mid > left) {
         curr_sum = nums[left]+nums[mid]+nums[right];
@@ -119,10 +156,8 @@ void cut(int nums[], int left, int right, int target, int *diff) {
             return;
         }
 
-#ifdef cut_simple       //should work
         if (curr_sum-target > 0) {
             right --;
-            //mid = left + 1;
             continue;
         } else if (curr_sum-target < 0) {
             walk_through(nums, left, right, target, diff);
@@ -134,41 +169,6 @@ void cut(int nums[], int left, int right, int target, int *diff) {
             mid = left+1;
             continue;
         }
-#else                   // still broken
-        if (curr_sum-target > 0) {
-            if (mid_from_left) {
-                right --;
-                mid = left + 1;
-                continue;
-            } else {
-                walk_through(nums, left, right, target, diff);
-                if (*diff == 0) {
-                    return ;
-                }
-
-                right --;
-                mid = left+1;
-                mid_from_left = 1;
-                continue;
-            }
-        } else if (curr_sum-target < 0) {
-            if (mid_from_left) {
-                walk_through(nums, left, right, target, diff);
-                if (*diff == 0) {
-                    return ;
-                }
-
-                left ++;
-                mid = right - 1;
-                mid_from_left = 0;
-                continue;
-            } else {
-                left ++;
-                mid = right -1;
-                continue;
-            }
-        }
-#endif
     }
     return;
 }
@@ -182,12 +182,11 @@ int threeSumClosest(int* nums, int numsSize, int target) {
 
     // do a quick sort
     quick_sort(nums, 0, numsSize-1);
-
-    // debug print for quick sort
     for (int i=0; i<numsSize; i++) {
         printf("%d, ", nums[i]);
     }
     printf("\n");
+    return 0;
     
 
     // the algurithm
@@ -201,8 +200,10 @@ int threeSumClosest(int* nums, int numsSize, int target) {
 
 
 int main () {
-    int nums[] = {-23, -13, -8, -3, 0, 1, 7, 11, 25, 30, 31};
-    int len = ARR_LEN(nums);
+    // init
+    //int nums[] = {-23, -13, -8, -3, 0, 1, 7, 11, 25, 30, 31};
+    int nums[] = {30, -23, 1, 11, -3, -8, 7, 0, 31, 13, 25};
+    len = ARR_LEN(nums);
     int target = 12;
     printf("target = %d\n", target);
     for (int i=0; i<len; i++) {
@@ -210,12 +211,7 @@ int main () {
     }
     printf("\n");
 
-    /*
-    quick_sort(nums, 0, len-1);
-    for (int i=0; i<len; i++) {
-        printf("%d, ", nums[i]);
-    }
-    printf("\n");
-    */
+
+    // entry point
     threeSumClosest(nums, len, target);
 }
