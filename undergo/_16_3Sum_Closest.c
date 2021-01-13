eb7655c57a628c5df5aff29e8f426e583050b9f9
alen6516 <alen6516@gmail.com>
2021-01-14 01:10:26 +0800

    update leetcode

--- undergo/_16_3Sum_Closest.c
+++ undergo/_16_3Sum_Closest.c
@@ -1,7 +1,7 @@
 /*
- * Runtime: 28 ms, faster than 34.72% of C online submissions for 3Sum Closest.
- * Memory Usage: 6.9 MB, less than 100.00% of C online submissions for 3Sum Closest.
- */
+Runtime: 12 ms, faster than 43.62% of C online submissions for 3Sum Closest.
+Memory Usage: 5.9 MB, less than 71.81% of C online submissions for 3Sum Closest.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -112,7 +112,7 @@ void check_and_replace(int target, int left, int mid, int right, int nums[], int
     int is_change = 0;                                              \
     int sum;                                                        \
     sum = nums[left] + nums[mid] + nums[right];                     \
-    if (abs(*diff) > abs(target-sum)) {                             \
+    if (abs(*diff) > abs(sum-target)) {                             \
         *diff = sum-target;                                         \
         is_change = 1;                                              \
     }                                                               \
@@ -123,11 +123,8 @@ void check_and_replace(int target, int left, int mid, int right, int nums[], int
 
 // walk through every element from left to right and check
 void walk_through(int nums[], int left, int right, int target, int *diff) {
-    if (right - left < 2) {
-        return ;
-    }
+    printf("walk start, left %d, right %d\n", left, right);
 
-    printf("walk start\n");
     for (int mid = left +1; mid<right; mid++) {
         check_and_replace(target, left, mid, right, nums, diff);
         if (*diff == 0) {
@@ -151,29 +148,55 @@ void cut(int nums[], int left, int right, int target, int *diff) {
     */
 
     int mid=left+1;
-    int curr_sum;
 
-    while(right > mid && mid > left) {
-        curr_sum = nums[left]+nums[mid]+nums[right];
-
+    // find the smallest right
+    check_and_replace(target, left, mid, right, nums, diff);
+    if (*diff == 0) {
+        return;
+    }
+    while ((nums[left]+nums[mid]+nums[right]) - target > 0) {
+        if (right - 1 == mid) {
+            break;
+        }
+        right --;
         check_and_replace(target, left, mid, right, nums, diff);
         if (*diff == 0) {
             return;
         }
+    }
+    printf("find the smallest right: %d\n", right);
 
-        if (curr_sum-target > 0) {
-            right --;
-            continue;
-        } else if (curr_sum-target < 0) {
-            walk_through(nums, left, right, target, diff);
+    mid = right - 1;
+
+    // find the largest left
+    check_and_replace(target, left, mid, right, nums, diff);
+    if (*diff == 0) {
+        return;
+    }
+    while ((nums[left]+nums[mid]+nums[right]) - target < 0) {
+        if (left + 1 == mid) {
+            break;
+        }
+        left ++;
+        check_and_replace(target, left, mid, right, nums, diff);
+        if (*diff == 0) {
+            return;
+        }
+    }
+    printf("find the largest left: %d\n", left);
+
+    mid = left+1;
+    while(right > mid && mid > left) {
+
+        for (int i=0; right-i > mid; i++) {
+            walk_through(nums, left, right-i, target, diff);
             if (*diff == 0) {
                 return ;
             }
-
-            left ++;
-            mid = left+1;
-            continue;
         }
+
+        left ++;
+        mid = left+1;
     }
     return;
 }
@@ -187,15 +210,18 @@ int threeSumClosest(int* nums, int numsSize, int target) {
 
     // do a quick sort
     quick_sort(nums, 0, numsSize-1);
+
+    // check quick sort result
     for (int i=0; i<numsSize; i++) {
         printf("%d, ", nums[i]);
     }
     printf("\n");
+    printf("====== sort ends =======\n\n");
     
 
-    // the algurithm
+    // do the algurithm
     int left=0, right = numsSize-1;
-    int diff = nums[numsSize-1] + nums[numsSize-2] + nums[numsSize-3];
+    int diff = (nums[0] + nums[1] + nums[numsSize-1]) - target;
     cut(nums, left, right, target, &diff);
     
     printf("sum = %d\n", target+diff);
@@ -206,9 +232,11 @@ int threeSumClosest(int* nums, int numsSize, int target) {
 int main () {
     // init
     //int nums[] = {-23, -13, -8, -3, 0, 1, 7, 11, 25, 30, 31};
-    int nums[] = {30, -23, 1, 11, -3, -8, 7, 0, 31, 13, 25};
+    //int nums[] = {30, -23, 1, 11, -3, -8, 7, 0, 31, 13, 25};
+    //int nums[] = {1,1,1,1};
+    int nums[] = {4,0,5,-5,3,3,0,-4,-5};
     len = ARR_LEN(nums);
-    int target = 12;
+    int target = -2;
     printf("target = %d\n", target);
     for (int i=0; i<len; i++) {
         printf("%d, ", nums[i]);
