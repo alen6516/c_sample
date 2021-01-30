e08ca53dba009667060407bcdd0cbff0ef80eb2c
alen6516 <alen6516@gmail.com>
2021-01-31 01:04:39 +0800

    update leetcode

--- undergo/17_Letter_Combinations_of_a_Phone_Number.c
+++ undergo/17_Letter_Combinations_of_a_Phone_Number.c
@@ -1,3 +1,7 @@
+/*
+ * Runtime: 0 ms, faster than 100.00% of C online submissions for Letter Combinations of a Phone Number.
+ * Memory Usage: 5.7 MB, less than 90.77% of C online submissions for Letter Combinations of a Phone Number.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +11,7 @@
  */
 char ** letterCombinations(char * digits, int* returnSize)
 {
-
     static const char * letter[] = {
-        [1] = "",
         [2] = "abc",
         [3] = "def",
         [4] = "ghl",
@@ -20,65 +22,74 @@ char ** letterCombinations(char * digits, int* returnSize)
         [9] = "wxyz",
     };
 
-    int len = strlen(digits);   // size of the input string
-    int arr_len = 1;            // size of the return array
-
-    printf("digits = %s\n", digits);
-    for (int i=0; i<len; i++) {
-        printf("%c = \"%s\"\n", digits[i], letter[digits[i]-('1'-1)]);
+    unsigned char digi_len = strlen(digits);   // size of the input string
+    
+    if (digi_len == 0) {
+        *returnSize = 0;
+        return (char**)0;
     }
 
-    for (int i=0; i<len; i++) {
+    int arr_len = 1;                // size of the return array
+
+    // debug
+    //printf("digits = %s\n", digits);
+    //for (int i=0; i<digi_len; i++) {
+    //    printf("%c = \"%s\"\n", digits[i], letter[digits[i]-('1'-1)]);
+    //}
+
+    // get return arr size
+    for (int i=0; i<digi_len; i++) {
         arr_len *= strlen(letter[digits[i]-('1'-1)]);
     }
+    *returnSize = arr_len;
 
-    printf("arr_len = %d\n", arr_len);
+    //printf("returnSize = %d\n", *returnSize);
 
-    int parr[len];              // position array, length is the size of the input string
+    unsigned char parr[digi_len];              // position array, length is the size of the input string
     memset(parr, 0, sizeof(parr));
 
     // alloc for return array
     char **ret = (char**) malloc(sizeof(char*)*arr_len);
 
     char *str;
-    int finish = 0;
+    unsigned char finish = 0;
 
     do {
         // fill string with the prepared parr
-        str = (char*) malloc(sizeof(char)*(len+1));
-        for (int w=0; w<len; w++) {
-            str[w] = *letter[digits[w]-('1'-1)]+parr[w];
+        str = (char*) malloc(sizeof(char)*(digi_len+1));
+        for (int w=0; w<digi_len; w++) {
+            str[w] = *letter[digits[w]-('1'-1)]+parr[w];          
         }
+        str[digi_len] = '\0';
         ret[finish++] = str;
         if (finish == arr_len) {
             break;
         }
 
-        // prepare
-        for (int p=0; p<len; p++) {
+        // prepare parr
+        for (int p=0; p<digi_len; p++) {
             if (parr[p] +1 == strlen(letter[digits[p]-('1'-1)])) {
                 parr[p] = 0;
                 continue;
             } else {
-            
                 parr[p] ++;
                 break;
             }
         }
     } while (1);
+
+    // debug
+    //for (int i=0; i<arr_len; i++) {
+    //    printf("%s\n", ret[i]);
+    //}
     
-    for (int i=0; i<arr_len; i++) {
-        printf("%s\n", ret[i]);
-    }
-
-    exit(0);
-
+    return ret;
 }
 
 int main ()
 {
 
-    const char *digit = "23";
+    const char *digit = "2";
     int returnSize = 0;
 
     char **ret = letterCombinations((char*)digit, &returnSize);
