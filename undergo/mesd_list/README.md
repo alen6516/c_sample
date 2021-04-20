d1f751346d4f39599c91088ca6837391f8ff6730
alen6516 <alen6516@gmail.com>
2021-04-20 16:06:00 +0800

    update mesd_list

--- undergo/mesd_list/README.md
+++ undergo/mesd_list/README.md
@@ -1,2 +1,5 @@
 multi productor, and single consumer
 no contention
+Finished:
+    func_test.c: test API
+    test.c: test with massive enqueue and dequeue to see if there is contention
--- undergo/mesd_list/test.c
+++ undergo/mesd_list/test.c
@@ -7,35 +7,78 @@
 #include "list.h"
 #include "node.h"
 
-#define LEN 2
+#define P_NUM 4
+#define T_NUM 1
+#define N_NUM 100
 
 list_t* list;
 
-int main ()
-{
-    node_t *root = node_init();
-    if (!root) {
-        printf("node mallc fail\n");
-    }
+_Atomic int take_sum = 0;
+_Atomic int put_sum = 0;
+_Atomic int taken = 0;
 
-    list = list_init((void*)root, node_get_ref_of_next(root));
-    printf("ref of root next: %p, list next: %p\n", &root->next, list->next);
+_Atomic int _putter_id;
+__thread int putter_id;
 
+_Atomic int _taker_id;
+__thread int taker_id;
+
+// multiple putter
+void* putter(void* arg) {
+    putter_id = ++ _putter_id;
     node_t* node;
-    void *out;
-    node = node_init();
-    if (!node) {
-        printf("node mallc fail\n");
+    for (int i=0; i<N_NUM; i++) {
+        node = node_init();
+        node->val = rand() % 10;
+        sleep(rand() % 1);
+        list_enqueue(list, (void*)node, (void**)&node->next);
+        put_sum += node->val;
+        printf("putter %d put val %d to list\n", putter_id, node->val);
     }
-    node->val = 1;
-
-    list_enqueue(list, (void*)node, (void**)&node->next);
-    printf("put %d, node addr: %p\n", node->val, node);
-    printf("root->next val: %p\n", node_get_next(list->root));
-    printf("ref of node next: %p, list next: %p\n", &node->next, list->next);
-
-    out = list_dequeue(list, node_get_next, node_link, node_get_ref_of_next);
-    printf("take %d, node addr: %p\n", ((node_t*)out)->val, out);
-    printf("root->next val: %p\n", node_get_next(list->root));
-    printf("ref of root next: %p, list next: %p\n", &root->next, list->next);
+    return NULL;
 }
+
+// only one taker
+void* taker(void* arg) {
+    taker_id = ++ _taker_id;
+    void* out;
+    while (taken< P_NUM*N_NUM) {
+        sleep(rand() % 1);
+        out = list_dequeue(list, node_get_next, node_link, node_get_ref_of_next);
+        if (!out) {
+            printf("taker %d take fail\n", taker_id);
+            continue;
+        }
+        printf("taker %d take val = %d\n", taker_id, ((node_t*)out)->val);
+        take_sum += ((node_t*)out)->val;
+        taken++;
+    }
+    printf("sum from taker = %d\n", take_sum);
+    printf("sum from putter = %d\n", put_sum);
+
+    return NULL;
+}
+
+int main () {
+
+    srand(time(NULL));
+
+    node_t* root = node_init();
+
+    list = list_init((void*)root, (void**)&root->next);
+    
+    pthread_t parr[P_NUM];
+    for (int i=0; i<P_NUM; i++) {
+        pthread_create(&parr[i], NULL, putter, NULL);
+    }
+
+    pthread_t tarr[T_NUM];
+    for (int i=0; i<T_NUM; i++) {
+        pthread_create(&tarr[i], NULL, taker, NULL);
+    }
+
+    for (int i=0; i<T_NUM; i++) {
+        pthread_join(tarr[i], NULL);
+    }
+}
+
