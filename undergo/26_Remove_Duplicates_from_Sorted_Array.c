cb5a2ee9dff88f26bec3b49dbee114caf520320e
alen6516 <alen6516@gmail.com>
2021-07-28 23:40:11 +0800

    update leet

--- undergo/26_Remove_Duplicates_from_Sorted_Array.c
+++ undergo/26_Remove_Duplicates_from_Sorted_Array.c
@@ -1,22 +1,41 @@
+/*
+Runtime: 12 ms, faster than 93.43% of C online submissions for Remove Duplicates from Sorted Array.
+Meomory Usage: 7.4 MB, less than 90.41% of C online submissions for Remove Duplicates from Sorted Array.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+int removeDuplicates(int* nums, int numsSize){
+    if (numsSize == 0 || numsSize == 1) return numsSize;
+    int curr = nums[0];
+    int *rp = NULL;
+    for (int i=1; i<numsSize; i++) {
+        if (nums[i] == curr) {
+            if (!rp) rp = &nums[i];
+        } else {
+            if (rp) {
+                *rp = nums[i];
+                rp += 1;
+            }
+            curr = nums[i];
+        }
+    }
+    return (rp) ? ((rp-nums)): numsSize;
+}
+
 
 /*
 Solution: start from the end of the array and detect duplicate numbers,
-once found, memmove to overrite the duplicated one.
+once found, memmove to overrite the duplicated one. Performance is bad.
 
 4 5 6 7 8 9 9 9 9 1 2 3
                 |<--->|
           9 1 2 3
           |<--->|  memmove
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-// 6, 7, 7, 7, 7, 9, 9, 9, 16, 20
-
-int removeDuplicates(int* nums, int numsSize){
+int _removeDuplicates(int* nums, int numsSize){
     if (numsSize == 0 || numsSize == 1) return numsSize;
 
     int size = 1;
@@ -48,26 +67,6 @@ int removeDuplicates(int* nums, int numsSize){
     }
     return (size == 0) ? numsSize : size;
 }
-
-
-int _removeDuplicates(int* nums, int numsSize){
-    if (numsSize == 0 || numsSize == 1) return numsSize;
-    int curr = nums[0];
-    int *rp = NULL;
-    for (int i=1; i<numsSize; i++) {
-        if (nums[i] == curr) {
-            if (!rp) rp = &nums[i];
-        } else {
-            if (rp) {
-                *rp = nums[i];
-                rp += 1;
-            }
-            curr = nums[i];
-        }
-    }
-    return (rp) ? ((rp-nums)): numsSize;
-}
-
 int main() {
     srand(time(NULL));
 
@@ -100,25 +99,25 @@ int main() {
     clock_t start, end, diff1, diff2;
     int ret_len;
 
-    //start = clock();
-    //ret_len = removeDuplicates(arr, size);
-    //end = clock();
-    //for (int i=0; i < ret_len; i++) {
-    //    printf("%d, ", arr[i]);
-    //}
-    //printf("\n");
-    //diff1 = end-start;
-    //printf("spend clock %ld, sec %f\n", end-start, (float)(end-start)/CLOCKS_PER_SEC);
+    start = clock();
+    ret_len = removeDuplicates(arr, size);		// straight and easy way
+    end = clock();
+    for (int i=0; i < ret_len; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+    diff1 = end-start;
+    printf("spend clock %ld, sec %f\n", end-start, (float)(end-start)/CLOCKS_PER_SEC);
 
-    //start = clock();
-    //ret_len = _removeDuplicates(arr2, size);
-    //end = clock();
-    //for (int i=0; i < ret_len; i++) {
-    //    printf("%d, ", arr[i]);
-    //}
-    //printf("\n");
-    //diff2 = end-start;
-    //printf("spend clock %ld, sec %f\n", end-start, (float)(end-start)/CLOCKS_PER_SEC);
+    start = clock();
+    ret_len = _removeDuplicates(arr2, size);	// memove way
+    end = clock();
+    for (int i=0; i < ret_len; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+    diff2 = end-start;
+    printf("spend clock %ld, sec %f\n", end-start, (float)(end-start)/CLOCKS_PER_SEC);
 
     free(arr);
     free(arr2);
---
+++ undergo/27_Remove_Element.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int removeElement(int* nums, int numsSize, int val){
+
+}
+
+int main ()
+{
+    
+    srand(time(NULL));
+    int len = (rand() % 20) + 5;
+    int *arr = (int*) malloc(sizeof(int)*len);
+}
