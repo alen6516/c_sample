de75dbdd00d9685cf76c7ba32eae3e5e980cee2a
alen6516 <alen6516@gmail.com>
2021-12-13 18:26:57 +0800

    update

--- undergo/33_Search_in_Rotated_Sorted_Array.c
+++ undergo/33_Search_in_Rotated_Sorted_Array.c
@@ -4,10 +4,60 @@
 #include <time.h>
 
 #define IS_ROTATE(arr, start, end)  (arr[start] > arr[end])
-#define IS_TARGET_IN_ARR(arr, target, start, end) (     \
-    (IS_ROTATE(arr, start, end) && (target > arr[start] || target < arr[end])) ||   \
-    (!IS_ROTATE(arr, start, end))
-)
+
+int _search(int* nums, int numsSize, int target)
+{
+    int start1, start2;
+    int ret = 0;
+    start1 = 0;
+
+    while (1) {
+        start2 = numsSize/2;
+
+        if (nums[start1] == target) {
+            //printf("1+%d\n", start1);
+            return ret+start1;
+        } else if (nums[start2-1] == target) {
+            //printf("2+%d\n", start2-1);
+            return ret+start2-1;
+        } else if (nums[start2] == target) {
+            //printf("3+%d\n", start2);
+            return ret+start2;
+        } else if (nums[numsSize-1] == target) {
+            //printf("4+%d\n", numsSize-1);
+            return ret+numsSize-1;    
+        }
+
+        if (IS_ROTATE(nums, start1, (start2-1)) &&
+            (target > nums[start1] || target < nums[start2-1])) {
+
+            nums = &nums[start1];
+            numsSize = start2-start1;
+        } else if (!IS_ROTATE(nums, start1, (start2-1)) &&
+            (target > nums[start1] && target < nums[start2-1])) {
+
+            nums = &nums[start1];
+            numsSize = start2-start1;
+        } else if (IS_ROTATE(nums, start2, (numsSize-1)) &&
+            (target > nums[start2] || target < nums[numsSize-1])) {
+
+            nums = &nums[start2];
+            numsSize = numsSize-start2;
+            ret += (start2-start1);
+            //printf("+%d\n", start2-start1);
+        } else if (!IS_ROTATE(nums, start2, (numsSize-1)) &&
+            (target > nums[start2] && target < nums[numsSize-1])) {
+
+            nums = &nums[start2];
+            numsSize = numsSize-start2;
+            ret += (start2-start1);
+            //printf("+%d\n", start2-start1);
+        } else {
+            //printf("-1\n");
+            return -1;
+        }
+    }
+}
 
 int search(int* nums, int numsSize, int target)
 {
@@ -16,30 +66,44 @@ int search(int* nums, int numsSize, int target)
     start2 = numsSize/2;
 
     if (nums[start1] == target) {
+        //printf("1+%d\n", start1);
         return start1;
     } else if (nums[start2-1] == target) {
+        //printf("2+%d\n", start2-1);
         return start2-1;
     } else if (nums[start2] == target) {
+        //printf("3+%d\n", start2);
         return start2;
     } else if (nums[numsSize-1] == target) {
+        //printf("4+%d\n", numsSize-1);
         return numsSize-1;    
     }
 
-    //   0   1   2  3  4  5  6  7  8  9  10 11 12 13  14
-    // [12, 13, 14, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ]
-    // target 9
-
+    int ret;
     if (IS_ROTATE(nums, start1, (start2-1)) &&
-        )
+        (target > nums[start1] || target < nums[start2-1])) {
 
-
-    if (!(target > nums[start2-1] && target > nums[start1])) {
         return search(&nums[start1], start2-start1, target);
+    } else if (!IS_ROTATE(nums, start1, (start2-1)) &&
+        (target > nums[start1] && target < nums[start2-1])) {
 
-    } else if (!(target > nums[start2] && target > nums[numsSize-1])) {
-        return (start2-start1) + search(&nums[start2], numsSize-start2, target);
+        return search(&nums[start1], start2-start1, target);
+    } else if (IS_ROTATE(nums, start2, (numsSize-1)) &&
+        (target > nums[start2] || target < nums[numsSize-1])) {
 
+        //printf("+%d\n", start2-start1);
+        ret = search(&nums[start2], numsSize-start2, target);
+        return (ret == -1) ? -1: ret+(start2-start1);
+        //return (start2-start1) + search(&nums[start2], numsSize-start2, target);
+    } else if (!IS_ROTATE(nums, start2, (numsSize-1)) &&
+        (target > nums[start2] && target < nums[numsSize-1])) {
+
+        //printf("+%d\n", start2-start1);
+        ret = search(&nums[start2], numsSize-start2, target);
+        return (ret == -1) ? -1: ret+(start2-start1);
+        //return (start2-start1) + search(&nums[start2], numsSize-start2, target);
     } else {
+        //printf("-1\n");
         return -1;
     }
 }
@@ -49,6 +113,7 @@ int main(int argc, char *argv[])
     int len;
     int rotate_idx;
     int target;
+    int target_idx;
     srand(time(NULL));
 
     if (argc >= 2) {
@@ -62,17 +127,31 @@ int main(int argc, char *argv[])
     } else {
         len = rand() % 15 + 1;
         rotate_idx = rand() % len;
-        target = rand() % len;
+        target_idx = rand() % len;
     }
 
     int *arr = (int *) malloc(sizeof(int)*len);
-    printf("len = %d, rot_idx = %d, target = %d\n", len, rotate_idx, target);
     printf("arr = [");
     for (int i=0; i<len; i++) {
-        arr[i] = (len-rotate_idx+i) % len;
+        if (i == rotate_idx) {
+            arr[i] == 0;
+        } else {
+            arr[i] = rand() % 3;
+            if (i > 0) {
+                arr[i] += arr[i-1];
+            }
+        }
+        if (i == target_idx) {
+            if (rand() % 2) {
+                target = arr[i];
+            } else {
+                target = arr[i]-1;
+            }
+        }
         printf("%d, ", arr[i]);
     }
     printf("]\n");
+    printf("len = %d, rot_idx = %d, target = %d\n", len, rotate_idx, target);
 
-    printf("idx = %d\n", search(arr, len, target));
+    printf("idx = %d\n", _search(arr, len, target));
 }
