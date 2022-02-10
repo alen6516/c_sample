545fc4be5b4aedff73736711349d34bffa20973a
alen6516 <alen6516@gmail.com>
2022-02-10 23:53:45 +0800

    update

--- util.h
+++ util.h
@@ -3,19 +3,21 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-typedef uint8_t bool_t;
+typedef uint8_t bool_t;     // should just use bool from stdbool.h
 typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-#define SIZE_OF_ARR(arr) ((int)(sizeof(arr)/sizeof(arr[0])))
-
-int print_nothing(const char *format, ...) {
-    //printf("in test\n");
-    return 0;
-}
+#define SWAP(a, b) ({   \
+    if ((a) != (b)) {   \
+        a = a^b;        \
+        b = a^b;        \
+        a = a^b;        \
+    }                   \
+})
 
 #ifdef DEBUG
 #define debug_printf(...) ((    \
@@ -25,12 +27,15 @@ int print_nothing(const char *format, ...) {
 #define debug_printf(...)
 #endif
 
+/* throw an error at compile time */
+#define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
+
 /* when condition occur, we must exit, but we don't want to */
 #define ASSERT_WARN(con, msg) do {                      \
     if (!!(con)==0) {                                   \
         printf("[ASSERT WARN] %s", (msg))               \
     }                                                   \
-} while (0)
+} while(0)
 
 /* when condition occur, we must exit immidiately */
 #define ASSERT_EXIT(con, msg) do {                      \
