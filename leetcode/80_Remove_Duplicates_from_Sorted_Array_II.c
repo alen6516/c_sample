5e5f324794e2c3f8678c35118d6c200ddc5dfd1c
alen6516 <alen6516@gmail.com>
2024-02-04 01:58:50 +0800

    leetcode 80

--- leetcode/80_Remove_Duplicates_from_Sorted_Array_II.c
+++ leetcode/80_Remove_Duplicates_from_Sorted_Array_II.c
@@ -6,6 +6,37 @@
 #include "utils/arr.h"
 #include "utils/utils.h"
 
+int removeDuplicates(int* nums, int numsSize) {
+
+    int rep = 0;        // how many repeats now
+    int i = 0;          // current processed idx
+    int prev;
+
+    while (i < numsSize) {
+        if (i > 0 && numsSize[i-1] == numsSize[i]) {
+            prev =  numsSize[i];
+        }
+
+        if (rep == 2) {
+            if (!find_idx)
+                find_idx = i+1;
+            while (numsSize[i] == numsSize[find_idx]) {
+                find_idx++;
+                if (find_idx == numsSize)
+                    return i-1;
+            }
+            numsSize[i] = numsSize[find_idx];
+            rep = 0;
+        }
+
+        i++;
+    }
+
+    return i;
+
+}
+
+#define MAX_CONSECUTIVE_NUM 3
 
 int main(int argc, char *argv[])
 {
@@ -20,19 +51,39 @@ int main(int argc, char *argv[])
 
     int *arr1, *arr2;
     arr1 = (int*) malloc(sizeof(int)*len);
-    arr2 = (int*) malloc(sizeof(int)*len*3);
+    arr2 = (int*) malloc(sizeof(int)*len*MAX_CONSECUTIVE_NUM);
 
+    int inc;
     for (int i=0; i<len; i++) {
+        inc = rand() % 3;
         if (i == 0) {
-            arr1[i] = rand() % 10;
+            arr1[i] = inc;
+        } else if (i > 1 && arr1[i-2] == arr1[i-1]) {
+            arr1[i] = arr1[i-1] + inc + 1;
         } else {
-            arr1[i] = rand() % 5 + arr1[i-1];
+            arr1[i] = arr1[i-1] + inc;
         }
     }
+    arr_show(arr1, len);
 
+    int rep;
+    int len2 = 0;
     for (int i=0; i<len; i++) {
-        while ()
-            arr2[i]
+        if (i > 0 && arr1[i-1] == arr1[i]) {
+            rep = (rand() % MAX_CONSECUTIVE_NUM) + 1;
+        } else {
+            rep = 1;
+        }
+        while (rep) {
+            arr2[len2] = arr1[i];
+            len2++;
+            rep--;
         }
     }
+    arr_show(arr2, len2);
+
+    int ret = removeDuplicates(arr2, len2);
+    printf("-------------\n");
+    printf("ret = %d\n", ret);
+    arr_show(arr2, ret);
 }
