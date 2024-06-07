da719e3dbe85951611d548dbd8e6b4460b4b4cb4
alen6516 <alen6516@gmail.com>
2024-06-07 11:10:38 +0800

    leetcode 380

---
+++ leetcode/380_Insert_Delete_GetRandom_O_1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include "utils/arr.h"
+#include "utils/utils.h"
+
+typedef struct {
+    int arr_len;
+    int curr_len;
+    int arr[0];
+} RandomizedSet;
+
+
+RandomizedSet* randomizedSetCreate() {
+    
+}
+
+bool randomizedSetInsert(RandomizedSet* obj, int val) {
+
+}
+
+bool randomizedSetRemove(RandomizedSet* obj, int val) {
+
+}
+
+int randomizedSetGetRandom(RandomizedSet* obj) {
+
+}
+
+void randomizedSetFree(RandomizedSet* obj) {
+
+}
+
+/**
+ * Your RandomizedSet struct will be instantiated and called as such:
+ * RandomizedSet* obj = randomizedSetCreate();
+ * bool param_1 = randomizedSetInsert(obj, val);
+
+ * bool param_2 = randomizedSetRemove(obj, val);
+
+ * int param_3 = randomizedSetGetRandom(obj);
+
+ * randomizedSetFree(obj);
+*/
+
+int main()
+{
+
+}
