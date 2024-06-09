b2aa3809ff42deba9a7cfa212a5f344582e3b0ec
alen6516 <alen6516@gmail.com>
2024-06-09 11:03:14 +0800

    leetcode 380

--- leetcode/380_Insert_Delete_GetRandom_O_1.c
+++ leetcode/380_Insert_Delete_GetRandom_O_1.c
@@ -6,23 +6,45 @@
 #include "utils/arr.h"
 #include "utils/utils.h"
 
+#define SIZE 10
+
 typedef struct {
     int arr_len;
     int curr_len;
-    int arr[0];
+    int *arr;
 } RandomizedSet;
 
 
 RandomizedSet* randomizedSetCreate() {
-    
+    RandomizedSet *ret;
+    ret = (RandomizedSet*) malloc(sizeof(RandomizedSet));
+    ret->arr = (int*) malloc(sizeof(int)*SIZE);
+    ret->arr_len = SIZE;
+    ret->curr_len = 0;
+    return ret;
 }
 
 bool randomizedSetInsert(RandomizedSet* obj, int val) {
-
+    if (obj->curr_len == obj->arr_len) {
+        int *arr = malloc(sizeof(int)*(obj->arr_len + SIZE));
+        memcpy(arr, obj->arr, sizeof(int)*obj->arr_len);
+        free(obj->arr);
+        obj->arr = arr;
+        obj->arr_len = obj->arr_len + SIZE;
+    }
+    obj->arr[obj->curr_len++] = val;
+    return 1;
 }
 
 bool randomizedSetRemove(RandomizedSet* obj, int val) {
-
+    for (int i=0; i < obj->arr_len; i++) {
+        if (val == obj->arr[i]) {
+            obj->arr[i] = obj->arr[obj->arr_len-1];
+            obj->arr_len --;
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int randomizedSetGetRandom(RandomizedSet* obj) {
@@ -30,7 +52,11 @@ int randomizedSetGetRandom(RandomizedSet* obj) {
 }
 
 void randomizedSetFree(RandomizedSet* obj) {
-
+    if (obj) {
+        if (obj->arr)
+            free(obj->arr);
+        free(obj);
+    }
 }
 
 /**
@@ -47,5 +73,5 @@ void randomizedSetFree(RandomizedSet* obj) {
 
 int main()
 {
-
+    
 }
