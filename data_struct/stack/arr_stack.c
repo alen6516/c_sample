2f9c5790308ef7ec0523ff04c19c4511d10dbb0a
alen6516 <alen6516@gmail.com>
2025-01-14 10:10:22 +0800

    update stack

--- data_struct/stack/arr_stack.c
+++ data_struct/stack/arr_stack.c
@@ -1,28 +1,91 @@
 
-#define SIZE 100
+#define SIZE 10
 
 typedef struct __stack {
-    int arr[100];
+    int size;
     int top;
+    int arr[0];
 } stack_t;
 
 // allocate and init stack
-stack_t *stack_init()
+stack_t *stack_create()
 {
+    stack_t *stack;
+    stack = (stack_t*) malloc(sizeof(stack_t)+SIZE*sizeof(int));
+    if (!stack) {
+        printf("fail to create stack\n");
+        return NULL;
+    }
+
+    stack->size = SIZE;
+    stack->top = -1;
+    return stack;
 }
 
 bool stack_is_empty(stack_t *stack)
+{
+    return (stack->top == -1);
+}
 
 bool stack_is_full(stack_t *stack)
+{
+    return (stack->top >= stack->size-1);
+}
 
-void stack_push(stack_t *stack, int val)
+stack_t *stack_expend(stack_t *stack)
+{
+    stack_t *new;
+    new = (stack_t*) malloc(sizeof(stack_t)+stack->size*2*sizeof(int));
+    if (!new) {
+        printf("fail to expend stack\n");
+        return stack;
+    }
 
-int stack_pop(stack_t *stack)
+    memcpy(new, stack, sizeof(stack_t)+stack->size*sizeof(int));
+    new->size = 2*stack->size;
+    free(stack);
+
+    return new;
+}
+
+bool stack_push(stack_t **stack, int val)
+{
+    stack_t *new;
+    if (stack_is_full(stack))
+        new = stack_expend(*stack);
+
+    if (new == stack) {
+        printf("stack push failed\n");
+        return -1;
+    } else {
+        stack = new;
+    }
+
+    stack->arr[++stack->top] = val;
+    return 0;
+}
+
+int stack_pop(stack_t *stack, int *ret)
+{
+    if (stack_is_empty(stack)) {
+        printf("stack pop failed\n");
+        return -1;
+    }
+
+    *ret = stack->arr[stack->top--];
+    return 0;
+}
 
 // return the top element without removing it
-int stack_peek(stack_t *stack)
+int stack_peek(stack_t *stack, int *ret)
 {
-    
+    if (stack_is_empty(stack)) {
+        printf("stack peek failed\n");
+        return -1;
+    }
+
+    *ret = stack->arr[stack->top];
+    return 0;
 }
 
 int main()
