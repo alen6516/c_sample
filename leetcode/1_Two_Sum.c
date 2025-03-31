ce7759934e9b6d4c055010f460d02dd856dbadc9
alen6516 <alen6516@gmail.com>
2025-04-01 00:41:39 +0800

    leetcode 1

--- leetcode/1_Two_Sum.c
+++ leetcode/1_Two_Sum.c
@@ -10,6 +10,65 @@
 #include "utils/arr.h"
 #include "utils/utils.h"
 
+#ifdef array
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+
+    int arr[10000] = {{-1}};
+    int *ret = malloc(sizeof(int)*2);
+    int val, complement;
+
+    for (int i=0; i<numsSize; i++) {
+        val = nums[i];
+        complement = target-val;
+        if (arr[complement] != -1) {
+            ret[0] = arr[complement];
+            ret[1] = i;
+            *returnSize = 2;
+            return ret;
+        } else {
+            arr[val] = i;
+        }
+    }
+    return NULL;
+}
+
+#elif def hash
+typedef struct hash_map_entry {
+    int key;
+    int val;
+    struct hash_map_entry *next;
+} HashMapEntry;
+
+typedef struct {
+    HashMapEntry *table;
+    int size;
+} HashMap;
+
+HashMap *createHashMap(int size)
+{
+    HashMap *map = (HashMap *)malloc(sizeof(HashMap));
+    map->size = size;
+    map->table = (HashMapEntry *)malloc(sizeof(HashMapEntry)*size);
+    for (int i=0; i<size; i++) {
+        map->table[i].key = -1;
+        map->table[i].next = NULL;
+    }
+    return map;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    
+    // init hashmap
+    HashMap *map = createHashMap(numsSize*2);
+
+    for (int i=0; i<numsSize; i++) {
+        key = nums[i] % numsSize;
+        if (map->table[key].key 
+    }
+}
+
+#else
 // Runtime 101 ms Beats 54.79%
 // Memory 8.58 MB Beats 92.56%
 
@@ -17,7 +76,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    
+ 
     int *ret = (int*) malloc(sizeof(int)*2);
 
     for (int i=0; i<numsSize-1; i++) {
@@ -34,3 +93,4 @@ out:
     *returnSize = 2;
     return ret;
 }
+#endif
