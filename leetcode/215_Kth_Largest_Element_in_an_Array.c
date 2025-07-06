f55d850c9f7f2c2d5daec1de3d7f54bb547f6adb
alen6516 <alen6516@gmail.com>
2025-07-06 17:38:41 +0800

    leetcode 215

--- leetcode/215_Kth_Largest_Element_in_an_Array.c
+++ leetcode/215_Kth_Largest_Element_in_an_Array.c
@@ -2,29 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
+#ifdef fail     // fail due to execution time out
 // maintain an array to track the first-k largerst element
 // len: current array len
 // size: array size
-void insert_to_arr(int *arr, int *len, int size, int val)
-{
-    int i=0;
-    for (; i < (*len); i++) {
-        if (val >= arr[i]) {
-            if (i < size-1)     // if not the last one, we need to memmove
-                memmove(&arr[i], &arr[i+1], sizeof(int)*(size-i-1));
-            arr[i] = val;
-            goto out;
-        }
-    }
-    if (i < size)   // if array not full, need to append
-        arr[i] = val;
-
-out:
-    if (*len < size) (*len)++;
-}
-
 void insert_to_arr(int *arr, int *len, int size, int val)
 {
     int i=0;
@@ -36,23 +17,42 @@ void insert_to_arr(int *arr, int *len, int size, int val)
 
     if (i < size) {     // if array not full, need to insert or append
         if (i < *len) { // need to memmove and insert
-            memmove(&arr[i], &arr[i+1], sizeof(int)*(size-i-1));
+            memmove(&arr[i+1], &arr[i], sizeof(int)*(size-i-1));
         }
         arr[i] = val;
         if (*len < size) (*len)++;
     }
-
 }
 
 int findKthLargest(int* nums, int numsSize, int k) {
 
     int *arr = (int*) malloc(sizeof(int)*k);
     int len = 0;
+    int ret;
     //memset(arr, 0, sizeof(int)*k);
 
-    for (int i=0; i<k; i++) {
+    for (int i=0; i<numsSize; i++) {
         insert_to_arr(arr, &len, k, nums[i]);
     }
 
-    return arr[k-1];
+    ret = arr[k-1];
+    free(arr);
+    return ret;
+}
+
+#else
+// bheap sort beforehand
+
+
+
+#endif
+
+int main()
+{
+    int arr[] = {1,0,6,2,5,8};
+    int len = 6;
+
+    int ret;
+    ret = findKthLargest(arr, 6, 4);
+    printf("%d\n", ret);
 }
