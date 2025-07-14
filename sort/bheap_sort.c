aa42b1b75cbabccf7d4394b62c14957e1da7fe4d
alen6516 <alen6516@gmail.com>
2025-07-14 12:23:21 +0800

    update sort/bheap_sort

--- sort/bheap_sort.c
+++ sort/bheap_sort.c
@@ -8,16 +8,56 @@ struct __bheap {
 typedef struct __bheap Bheap;
 
 
-Bheap *bheap_create();
-static Bheap* bheap_expand();
+Bheap *bheap_create(int);
+static Bheap* bheap_expand(Bheap*);
 static void bheap_topdown();
 static void bheap_bottomup();
 void bheap_sort
+void bheap_push();
+void bheap_pop();
 
 #define SIZE 10
 
-Bheap *bheap_create()
+Bheap *bheap_create(int size)
 {
     Bheap *ret;
-    ret = (Bheap *)malloc(sizeof(Bheap)+);
+    if (size == 0) size = SIZE;
+    ret = (Bheap *)malloc(sizeof(Bheap)+size*sizeof(int));
+    ret->size = size;
+    ret->len = 0;
+    return ret;
+}
+
+void bheap_push(Bheap *bheap, int val)
+{
+    if (bheap->size == bheap->len) {
+        // full
+        bheap = bheap_expand(Bheap *bheap);
+    }
+
+    bheap->arr[bheap->len-1] = val;
+    bheap->len ++;
+    bheap_bottomup(bheap);
+}
+
+int bheap_pop(Bheap *bheap)
+{
+    if (bheap->len == 0) return -1;
+
+    int ret = bheap->arr[0];
+    bheap->arr[0] = bheap->arr[bheap->len-1];
+    bheap->len --;
+    bheap_topdown(bheap);
+    return ret;
+}
+
+static Bheap *bheap_expand(Bheap *bheap)
+{
+    int size = bheap->size * 2;
+    Bheap *ret = (Bheap *)malloc(sizeof(Bheap)+size*sizeof(int));
+    ret->size = size;
+    ret->len = bheap->len;
+    memcpy(ret->arr, bheap->arr, sizeof(int)*bheap->len);
+    free(bheap);
+    return ret;
 }
