b61daa22081a4bf24715bffac5e097460ae1e050
alen6516 <alen6516@gmail.com>
2025-07-17 10:32:43 +0800

    sort/bheap_sort

--- sort/bheap_sort.c
+++ sort/bheap_sort.c
@@ -5,10 +5,26 @@ void heapify();
 void bheap_sort(int *arr, int len);
 void show_bheap_sort(int *_arr, int len);
 
+void heapify(int *arr, int len, int i)
+{
+    // compare i with its left&right child and swap
+    int right = (i+1)*2;
+    int left = (i+1)*2-1;
+}
+
 void bheap_sort(int *arr, int len)
 {
     // 1. build max heap
+    int i;  // start with i who is the last node having child
+    i = len/2-1;
+    for (i=len/2-1; i>=0; i++) {
+        heapify(int *arr, len, i);
+    }
+
     // 2. swap root with last idx, and heapify with len-1 again
+    for (
+    swap(arr[0], arr[len-
+
 }
 
 void show_bheap_sort(int *_arr, int len)
