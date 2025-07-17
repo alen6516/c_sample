b4b197a7680e10fd669764aa4811343b0ec39ec1
alen6516 <alen6516@gmail.com>
2025-07-18 00:20:49 +0800

    update sort/bheap_sort

--- sort/bheap_sort.c
+++ sort/bheap_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "../utils/utils.h"
 
 void heapify();
 void bheap_sort(int *arr, int len);
@@ -7,24 +8,33 @@ void show_bheap_sort(int *_arr, int len);
 
 void heapify(int *arr, int len, int i)
 {
-    // compare i with its left&right child and swap
+    // compare i with its left & right child and swap
     int right = (i+1)*2;
     int left = (i+1)*2-1;
+    int largest = i;
+
+    if (right < len && arr[largest] < arr[right])
+        largest = right;
+
+    if (left < len && arr[largest] < arr[left])
+        largest = left;
+
+    SWAP(arr[largest], arr[i]);
 }
 
 void bheap_sort(int *arr, int len)
 {
-    // 1. build max heap
-    int i;  // start with i who is the last node having child
-    i = len/2-1;
-    for (i=len/2-1; i>=0; i++) {
-        heapify(int *arr, len, i);
+    int i = 0, j;
+    while (i<len-1) {
+        // j is the last idx having left or right child
+        for (j=(len-i)/2-1; j>=0; j--) {
+            heapify(arr, len-i, j);
+        }
+
+        // swap root with current last idx, so the largest goes end
+        SWAP(arr[0], arr[len-i-1]);
+        i++;    // i means how many elements processed
     }
-
-    // 2. swap root with last idx, and heapify with len-1 again
-    for (
-    swap(arr[0], arr[len-
-
 }
 
 void show_bheap_sort(int *_arr, int len)
